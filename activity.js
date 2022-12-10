d1a2d6e3fbf6a555405546810da32abb0735683d
week1-day1 <i.yang.355@westcliff.edu>
2022-12-10 11:59:18 -0800

    three commit

--- activity.js
+++ activity.js
@@ -1,8 +1,9 @@
-$(document).ready(function() {
-var cells = document.querySelectorAll("#position td");
-for (var i = 0; i < cells.length; i++) {
-  cells[i].addEventListener("click", function() {
-    console.log(this.innerHTML);
+$(document).ready(function () {
+  $("td").click(function () {
+    var activities = $(this).text();
+    // if activities does not equal Not Available which is pass as a str
+    if (activities != "Not Available") {
+      $(this).toggleClass("selected");
+    }
   });
-}
-})
+});
